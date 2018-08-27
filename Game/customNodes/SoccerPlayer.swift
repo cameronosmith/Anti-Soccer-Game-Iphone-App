930224ee07b6ca562db53e27b89c7d58c7e1df00
cameronosmith <31725104+cameronosmith@users.noreply.github.com>
2018-08-27 16:00:48 -0700

    Update SoccerPlayer.swift

--- Game/customNodes/SoccerPlayer.swift
+++ Game/customNodes/SoccerPlayer.swift
@@ -2,7 +2,7 @@
 //  Player.swift
 //  antiSoccerGame2
 //
-//  Created by Rachel Corren on 7/9/18.
+//  Created by Cameron Smith on 7/9/18.
 //  Copyright © 2018 com.cameronSmith. All rights reserved.
 //
 
